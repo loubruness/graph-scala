19c9d17c471be63eb686a79f9c78c36ad2981679
ArthB94 <80051198+ArthB94@users.noreply.github.com>
2024-07-03 09:07:37 +0200

    init

--- src/main/scala/ZioJson.scala
+++ src/main/scala/ZioJson.scala
@@ -5,5 +5,5 @@ import zio.json._
 implicit val directedEdgeEncoder: JsonEncoder[DirectedEdge[String]] = DeriveJsonEncoder.gen[DirectedEdge[String]]
 implicit val directedEdgeDecoder: JsonDecoder[DirectedEdge[String]] = DeriveJsonDecoder.gen[DirectedEdge[String]]
 
-implicit val directedGraphEncoder: JsonEncoder[Directed_Graph[String]] = DeriveJsonEncoder.gen[Directed_Graph[String]]
-implicit val directedGraphDecoder: JsonDecoder[Directed_Graph[String]] = DeriveJsonDecoder.gen[Directed_Graph[String]]
+// implicit val directedGraphEncoder: JsonEncoder[Directed_Graph[String]] = DeriveJsonEncoder.gen[Directed_Graph[String]]
+// implicit val directedGraphDecoder: JsonDecoder[Directed_Graph[String]] = DeriveJsonDecoder.gen[Directed_Graph[String]]
