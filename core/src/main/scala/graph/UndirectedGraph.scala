6273fa782140c25cf520dda8b5f11d53bdda88da
ArthB94 <80051198+ArthB94@users.noreply.github.com>
2024-07-03 14:54:32 +0200

    UndirectedGraph

--- core/src/main/scala/graph/UndirectedGraph.scala
+++ core/src/main/scala/graph/UndirectedGraph.scala
@@ -2,10 +2,10 @@ package graph
 
 case class UndirectedEdge[V](v1: V, v2: V)
 
-final case class UnDirectedGraph[V](val vertices: Set[V], val edges: Set[UndirectedEdge[V]]) extends Graph[V, UndirectedEdge[V]] {
+final case class UndirectedGraph[V](val vertices: Set[V], val edges: Set[UndirectedEdge[V]]) extends Graph[V, UndirectedEdge[V]] {
   def neighbors(vertex: V): Set[V] = edges.collect { case UndirectedEdge(`vertex`, other) => other } ++ edges.collect { case UndirectedEdge(other, `vertex`) => other }
 
-  def addEdge(edge: UndirectedEdge[V]): UnDirectedGraph[V] = new UnDirectedGraph(vertices ++ Set(edge.v1, edge.v2), edges + edge)
+  def addEdge(edge: UndirectedEdge[V]): UndirectedGraph[V] = new UndirectedGraph(vertices ++ Set(edge.v1, edge.v2), edges + edge)
 
-  def removeEdge(edge: UndirectedEdge[V]): UnDirectedGraph[V] = new UnDirectedGraph(vertices, edges - edge)
+  def removeEdge(edge: UndirectedEdge[V]): UndirectedGraph[V] = new UndirectedGraph(vertices, edges - edge)
 }
